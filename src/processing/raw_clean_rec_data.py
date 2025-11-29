104235968cc3d695b3f791cc2dae1e3f84793039
Minh_Nguyen <136102473+Helooeverybody@users.noreply.github.com>
2025-11-29 18:42:09 +0700

    Update silver

--- src/processing/raw_clean_rec_data.py
+++ src/processing/raw_clean_rec_data.py
@@ -1,10 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StringType, StructType, StructField, ArrayType, MapType
+from pyspark.sql.types import StringType, DoubleType
 import re
 
-input_path = "data/top_10_games_reviews.json"   
-output_path = "cleaned_reviews.csv"              
+input_path = "s3a://spark-scripts/570.jsonl"
+output_path = "s3a://spark-scripts/570.parquet"              
 def clean_text(text):
     if text is None:
         return ""
@@ -15,36 +15,30 @@ def clean_text(text):
 
 spark = SparkSession.builder.appName("CleanSteamReviews").getOrCreate()
 clean_text_udf = F.udf(clean_text, StringType())
+
 df_raw = spark.read.option("multiline", True).json(input_path)
-df_exploded = (
-    df_raw
-    .select(F.explode(F.map_entries(F.col("*"))).alias("kv"))
-    .select(
-        F.col("kv.key").alias("appid"),
-        F.col("kv.value").alias("reviews")
-    )
+
+author_cols = []
+if "author" in df_raw.columns:
+    author_struct_cols = df_raw.select("author.*").columns
+    author_cols = [F.col(f"author.{c}").alias(f"author_{c}") for c in author_struct_cols]
+
+df_flat = df_raw.select(
+    F.coalesce(F.col("recommendationid"), F.lit("unknown")).alias("recommendationid"),
+    F.coalesce(F.col("review"), F.lit("")).alias("review"),
+    *author_cols
 )
 
-# Explode each list of reviews
-df_reviews = df_exploded.select(
-    F.col("appid"),
-    F.explode("reviews").alias("review")
-)
-
-# Flatten the 'author' field into top-level columns
-df_flat = df_reviews.select(
-    "appid",
-    *[F.col(f"review.{c}").alias(c) for c in df_reviews.select("review.*").columns if c != "author"],
-    *[F.col(f"review.author.{c}").alias(f"author_{c}") for c in df_reviews.select("review.author.*").columns]
-)
-timestamp_cols = ["timestamp_created", "timestamp_updated", "author_last_played"]
+timestamp_cols = ["timestamp_created", "timestamp_updated", "author_last_played", "author_last_played"]
 for col in timestamp_cols:
     if col in df_flat.columns:
-        df_flat = df_flat.withColumn(col, F.to_timestamp(F.col(col).cast("double")))
+        df_flat = df_flat.withColumn(col, F.to_timestamp(F.col(col).cast(DoubleType())))
+
 df_cleaned = df_flat.withColumn("review", clean_text_udf(F.col("review")))
-subset_cols = [col for col in ["author_steamid", "review", "appid"] if col in df_cleaned.columns]
+
+subset_cols = [col for col in ["author_steamid", "review", "recommendationid"] if col in df_cleaned.columns]
 df_dedup = df_cleaned.dropDuplicates(subset=subset_cols)
 
-#save
-df_dedup.write.mode("overwrite").option("header", True).csv(output_path)
+
+df_dedup.write.mode("overwrite").parquet(output_path)
 print(f"Cleaned data saved to {output_path}")
